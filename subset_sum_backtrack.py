914af4e74bdcc72953f2db5a96d2922ef8fdd360
shadowfaxthewhitehorse <66239320+shadowfaxthewhitehorse@users.noreply.github.com>
2023-02-20 14:40:25 +0530

    Update subset_sum_backtrack.py

--- subset_sum_backtrack.py
+++ subset_sum_backtrack.py
@@ -1,3 +1,11 @@
+# solve the subset sum problem
+#
+# The problem is as follows: given a set S of n positive integers, and a positive integer d, 
+# write a program to find that subset of S that sums up to d.
+#
+# This implementation uses backtracking.
+#
+# function subset_sum_backtrack()
 def subset_sum_backtrack(S, d):
     n = len(S)
     path = []
@@ -24,6 +32,25 @@ def subset_sum_backtrack(S, d):
 
     backtrack(0, 0)
     return result
+
+# DEVNOTES
+#
+# The problem is as follows: given a set S of n positive integers, and a positive integer d, 
+# write a program to find that subset of S that sums up to d.
+# 
+# S = {s_0, s_1, ..., s_n}
+#
+# The program defines a backtracking function backtrack, which takes two arguments: the starting index start and the current sum sum. The function 
+# checks if the current sum equals the target sum d. If so, it appends the current path to the result array. If the current sum is greater than d 
+# or the starting index equals the length of the input array, the function returns without modifying the result array.
+#
+# The function then recursively calls itself twice, once to include the current element in the subset and once to exclude it. The function updates 
+# the current sum and the current path accordingly.
+#
+# Finally, the program calls subset_sum_backtrack with the input array S and the target sum d. It prints the resulting subset to the console.
+#
+# Note that this backtracking algorithm can be visualized as a tree, with each level i either adding element # i (s_i) or not adding it.
+# 
 
 # Example usage
 S = [1, 2, 3, 4, 5]
